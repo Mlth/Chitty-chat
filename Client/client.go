c8d943a96ff70427b00fe96960ba551bda0d0d8a
mlth <mlth@itu.dk>
2022-10-19 14:40:23 +0200

    implemented joinServer and leaveServer methods

--- Client/client.go
+++ Client/client.go
@@ -1,1 +1,56 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	chat "github.com/Mlth/Chitty-chat/proto"
+	"google.golang.org/grpc"
+)
+
+var id int32 = 0
+var name string = ""
+
+func main() {
+	// Creat a virtual RPC Client Connection on port  9080 WithInsecure (because  of http)
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial(":9080", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not connect: %s", err)
+	}
+
+	// Defer means: When this function returns, call this method (meaing, one main is done, close connection)
+	defer conn.Close()
+
+	fmt.Print("Write your name: ")
+	fmt.Scan(&name)
+
+	//  Create new Client from generated gRPC code from proto
+	c := chat.NewChatClient(conn)
+
+	JoinServer(c)
+}
+
+func JoinServer(c chat.ChatClient) {
+	// Between the curly brackets are nothing, because the .proto file expects no input.
+	message := chat.WrittenMessage{Name: name}
+
+	response, err := c.JoinServer(context.Background(), &message)
+	if err != nil {
+		log.Fatalf("Error when calling GetTime: %s", err)
+	}
+
+	var responseName string = response.Name
+	id = response.Id
+	log.Printf("%s has joined the server with id: %d", responseName, id)
+}
+
+func SendMessage(c chat.ChatClient, inputMessage string) {
+	message := chat.WrittenMessage{Name: name, Message: inputMessage, TimeStamp: "", Id: id}
+
+	_, err := c.SendMessage(context.Background(), &message)
+	if err != nil {
+		log.Fatalf("Error when calling GetTime: %s", err)
+	}
+}
--- Server/server.go
+++ Server/server.go
@@ -1,1 +1,46 @@
 package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	chat "github.com/Mlth/Chitty-chat/proto"
+	"google.golang.org/grpc"
+)
+
+var idCount int32 = 0
+
+type ChittyChatServer struct {
+	chat.UnimplementedChatServer
+}
+
+// Might give two users the same id
+func (s *ChittyChatServer) JoinServer(ctx context.Context, in *chat.WrittenMessage) (*chat.WrittenMessage, error) {
+	log.Printf("%s joined server. They have been assigned id: %d", in.Name, idCount)
+	idCount++
+	return &chat.WrittenMessage{Name: in.Name, Message: " ", TimeStamp: " ", Id: idCount - 1}, nil
+}
+
+func (s *ChittyChatServer) LeaveServer(ctx context.Context, in *chat.WrittenMessage) (*chat.WrittenMessage, error) {
+	log.Printf("%s left the server", in.Name)
+	return &chat.WrittenMessage{Name: in.Name, Message: " ", TimeStamp: " ", Id: in.Id}, nil
+}
+
+func (s *ChittyChatServer) SendMessage(ctx context.Context, in *chat.WrittenMessage) (*chat.WrittenMessage, error) {
+
+}
+
+func main() {
+	// Create listener tcp on port 9080
+	list, err := net.Listen("tcp", ":9080")
+	if err != nil {
+		log.Fatalf("Failed to listen on port 9080: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	chat.RegisterChatServer(grpcServer, &ChittyChatServer{})
+
+	if err := grpcServer.Serve(list); err != nil {
+		log.Fatalf("failed to server %v", err)
+	}
+}
